39a23872b9d99af2b2526a137735a5126d236de2
pancakescrawlspace <59287541+pancakescrawlspace@users.noreply.github.com>
2022-05-18 00:23:37 +0200

    Update frans.py

--- frans.py
+++ frans.py
@@ -14,7 +14,7 @@ total = np.zeros(NUM + 5);
 total[5] = 1;
 
 for i in range(6, NUM + 5):
-    total[i] = 1/6 * sum(total[i - 6 : i]);
+    total[i] = sum(total[i - 6 : i]) / 6;
     print("Kans op een tussentotaal van " + str(i - 5) + ": " + str(total[i]));
 
 print("\nKans op een solo: " + str(sum(total[5 : NUM + 5]) / NUM / 6));
