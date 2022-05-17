88dae64fe63f6771bc988037a17c277b1a52352e
pancakescrawlspace <59287541+pancakescrawlspace@users.noreply.github.com>
2022-05-18 00:15:32 +0200

    Add files via upload

---
+++ frans.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 17 16:44:32 2022
+
+@author: rene
+"""
+
+import numpy as np;
+
+NUM = 8;
+
+total = np.zeros(NUM + 5);
+total[5] = 1;
+
+for i in range(6, NUM + 5):
+    total[i] = 1/6 * sum(total[i-6:i]);
+    print("Kans op een tussentotaal van " + str(i - 5) + ": " + str(total[i]));
+
+print();
+print("Kans op een solo: " + str(sum(total[5:NUM+5]) / NUM * 1/6));
